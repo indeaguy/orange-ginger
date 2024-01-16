18a6ac9b49e0eab83614bfb85fade93cfdb6e785
indeaguy <davide.mcguigan@gmail.com>
2024-01-15 20:31:53 -0400

    Adding notify event emitter

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './product-alerts.component.html',
-  styleUrl: './product-alerts.component.css'
+  styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
 
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
